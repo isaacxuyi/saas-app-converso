f3b766a8c3b6eac47e0481c8fe77949d3cec345c
Martins uhunmwangho <martinsworkacc@gmail.com>
2026-05-02 00:08:17 +0100

    update companion form

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -1,9 +1,24 @@
+import Link from "next/link"
+import Image from "next/image"
+
 const CTA = () => {
   return (
-    <div>
-      CTA
-    </div>
+    <section className="cta-section">
+      <div className="cta-badge">Start Learning Your way</div>
+      <h2 className="text-3xl font-bold">
+        Build and Personalize Your Learning Companion
+      </h2>
+      <p>pick a name, subject, voice & personality - and start learning through voice conversations that feel natural, interactive and fun</p>
+      <Image src="images/cta.svg" alt="cta" height={362} width={232} />
+      <button className="btn-primary">
+        <Image src="/icons/plus.svg" alt="plus" height={12} width={12} />
+        <Link href="/companions/new">
+        <p>Build a new companion</p>
+        </Link>
+      </button>
+    </section>
   )
 }
 
 export default CTA
+ 
---
+++ components/CompanionForm.tsx
@@ -0,0 +1,183 @@
+"use client"
+
+import { z } from "zod"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Button } from "@/components/ui/button"
+
+import {
+  Form,
+  useFormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormDescription,
+  FormMessage,
+  FormField,
+} from "@/components/ui/form"
+
+const formSchema = z.object({
+    name: z.string().min(1, { message: "Companion is required" }).max(50),
+    subject: z.string().min(1, { message: "subject is required" }).max(50),
+    topic: z.string().min(1, { message: "topic is required" }).max(50),
+    voice: z.string().min(1, { message: "voice is required" }).max(50),
+    style: z.string().min(1, { message: "style is required" }).max(50),
+    duration: z.coerce.number().min(1, { message: "duration is required" }).max(50),
+}) 
+
+
+const CompanionForm = () => {
+    const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+        name: '',
+        subject: "",
+        topic: "",
+        voice: "",
+        style: "",
+        duration: 15,
+    },
+  })
+ 
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    // Do something with the form values.
+    console.log(values)
+  }
+
+  return (
+    <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                    <FormItem>
+                        <FormLabel>Companion name</FormLabel>
+                        <FormControl>
+                            <input
+                                className="w-full rounded-md border border-slate-200 bg-background px-3 py-2 text-sm shadow-sm outline-none transition focus:border-slate-400 focus:ring-2 focus:ring-slate-200 disabled:cursor-not-allowed disabled:opacity-50"
+                                placeholder="Enter your companion name"
+                                {...field}
+                            />
+                        </FormControl>
+                        <FormDescription>
+                            This is your public display name.
+                        </FormDescription>
+                        <FormMessage />
+                    </FormItem>
+                )}
+                
+            />
+             <FormField
+                control={form.control}
+                name="subject"
+                render={({ field }) => (
+                    <FormItem>
+                        <FormLabel>Subject</FormLabel>
+                        <FormControl>
+                            <input
+                                className="w-full rounded-md border border-slate-200 bg-background px-3 py-2 text-sm shadow-sm outline-none transition focus:border-slate-400 focus:ring-2 focus:ring-slate-200 disabled:cursor-not-allowed disabled:opacity-50"
+                                placeholder="Enter your companion name"
+                                {...field}
+                            />
+                        </FormControl>
+                        <FormDescription>
+                            This is your public display name.
+                        </FormDescription>
+                        <FormMessage />
+                    </FormItem>
+                )}
+                
+            />
+             <FormField
+                control={form.control}
+                name="topic"
+                render={({ field }) => (
+                    <FormItem>
+                        <FormLabel>Topic</FormLabel>
+                        <FormControl>
+                            <input
+                                className="w-full rounded-md border border-slate-200 bg-background px-3 py-2 text-sm shadow-sm outline-none transition focus:border-slate-400 focus:ring-2 focus:ring-slate-200 disabled:cursor-not-allowed disabled:opacity-50"
+                                placeholder="Enter your companion name"
+                                {...field}
+                            />
+                        </FormControl>
+                        <FormDescription>
+                            This is your public display name.
+                        </FormDescription>
+                        <FormMessage />
+                    </FormItem>
+                )}
+                
+            />
+             <FormField
+                control={form.control}
+                name="voice"
+                render={({ field }) => (
+                    <FormItem>
+                        <FormLabel>Voice</FormLabel>
+                        <FormControl>
+                            <input
+                                className="w-full rounded-md border border-slate-200 bg-background px-3 py-2 text-sm shadow-sm outline-none transition focus:border-slate-400 focus:ring-2 focus:ring-slate-200 disabled:cursor-not-allowed disabled:opacity-50"
+                                placeholder="Enter your voice"
+                                {...field}
+                            />
+                        </FormControl>
+                        <FormDescription>
+                            This is your public display name.
+                        </FormDescription>
+                        <FormMessage />
+                    </FormItem>
+                )}
+                
+            />
+             <FormField
+                control={form.control}
+                name="style"
+                render={({ field }) => (
+                    <FormItem>
+                        <FormLabel>Style</FormLabel>
+                        <FormControl>
+                            <input
+                                className="w-full rounded-md border border-slate-200 bg-background px-3 py-2 text-sm shadow-sm outline-none transition focus:border-slate-400 focus:ring-2 focus:ring-slate-200 disabled:cursor-not-allowed disabled:opacity-50"
+                                placeholder="Enter your style"
+                                {...field}
+                            />
+                        </FormControl>
+                        <FormDescription>
+                            This is your public display name.
+                        </FormDescription>
+                        <FormMessage />
+                    </FormItem>
+                )}
+                
+            />
+             <FormField
+                control={form.control}
+                name="Duration"
+                render={({ field }) => (
+                    <FormItem>
+                        <FormLabel>Duration</FormLabel>
+                        <FormControl>
+                            <input
+                                className="w-full rounded-md border border-slate-200 bg-background px-3 py-2 text-sm shadow-sm outline-none transition focus:border-slate-400 focus:ring-2 focus:ring-slate-200 disabled:cursor-not-allowed disabled:opacity-50"
+                                placeholder="Enter the duration"
+                                {...field}
+                            />
+                        </FormControl>
+                        <FormDescription>
+                            This is your public display name.
+                        </FormDescription>
+                        <FormMessage />
+                    </FormItem>
+                )}
+                
+            />
+            <Button type="submit">submit</Button>
+        </form>
+    </Form>
+    
+  )
+}
+
+export default CompanionForm
